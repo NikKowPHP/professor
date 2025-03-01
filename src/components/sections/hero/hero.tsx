b04eb8710fae8f597d0993a7619af0b6beab610b
NikKowPHP <nik.kow@outlook.com>
2025-03-01 17:27:15 +0100

    finish hero

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRight } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -16,7 +17,7 @@ export const HeroSection = async () => {
 
   return (
     <section
-      className="flex grid grid-cols-1 sm:grid-cols-2 justify-between  py-[40px]  border border-red-500"
+      className=" grid grid-cols-1 sm:grid-cols-2 sm:gap-x-[150px]   sm:py-[40px]  border border-red-500"
       itemScope
       itemType="https://schema.org/WebPageElement"
     >
@@ -31,25 +32,25 @@ export const HeroSection = async () => {
         />
       </div>
       {/* Optimize heading for LCP */}
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col  gap-[20px]">
         <div className="border border-blue-500 flex flex-col items-center gap-[40px]">
           <h1
-            className="font-bold text-[36px] sm:text-[36px] lg:text-[36px] leading-[1.1] font-bold  mb-2 text-center max-w-[1200px] mx-auto italic "
+            className="font-bold text-[36px] sm:text-[36px] lg:text-[36px] leading-[1.1] font-bold   text-center max-w-[1200px] mx-auto italic "
             itemProp="headline"
           >
             Prof. Michael Friebe, PhD
           </h1>
           <h2
-            className="font-normal text-[36px] sm:text-[36px] lg:text-[36px] leading-[1.1] font-normal mb-2 text-center max-w-[1200px] mx-auto"
+            className="font-normal text-[36px] sm:text-[36px] lg:text-[36px] leading-[1.1] font-normal  text-center max-w-[1200px] mx-auto"
             itemProp="headline"
           >
             HealthTEC: Creating Innovations for Enterprises and New Ventures +
           </h2>
           <h3 className="flex flex-col items-center gap-[10px]">
             <span>{massiveTransformativePurpose()}</span>
-            <span>Health Democratise Enabler</span>
+            <span className="font-bold ">Health Democratise Enabler</span>
           </h3>
-          <p>
+          <p className="text-[16px]">
             Prof. Michael Friebe has extensive experience in medical technology,
             with a focus on innovation, entrepreneurship, and image-guided
             therapies. He has a strong background as a founder, innovator, CEO,
@@ -58,7 +59,7 @@ export const HeroSection = async () => {
         </div>
         {/* cta */}
         <div>
-          <Link href="/about">My Investments</Link>
+          <Link href="/about" className="flex items-center gap-[10px]">My Investments <ArrowUpRight className="w-6 h-6" /></Link>
         </div>
       </div>
 
