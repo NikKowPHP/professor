352655115c9d8354a578c69e82757d0ded4fb6e9
NikKowPHP <nik.kow@outlook.com>
2025-03-04 10:28:57 +0100

    remove unused components

--- src/helpers/componentsLoad.tsx
+++ src/helpers/componentsLoad.tsx
@@ -38,67 +38,6 @@ export const YoutubeSection = dynamic(
   }
 )
 
-export const WhyUs = dynamic(
-  () => import('@/components/sections/why-us/why-us').then((mod) => mod.WhyUs),
-  {
-    ssr: true,
-  }
-)
-
-export const Philosophy = dynamic(
-  () =>
-    import('@/components/sections/philosophy/philosophy').then(
-      (mod) => mod.Philosophy
-    ),
-  {
-    ssr: true,
-  }
-)
-
-export const StayInformed = dynamic(
-  () =>
-    import('@/components/sections/stay-informed/stay-informed').then(
-      (mod) => mod.StayInformed
-    ),
-  {
-    ssr: true,
-  }
-)
-
-export const Faq = dynamic(
-  () => import('@/components/sections/faq/faq').then((mod) => mod.Faq),
-  {
-    loading: () => (
-      <div className="min-h-[300px] sm:min-h-[350px] lg:min-h-[400px]" />
-    ),
-    ssr: true,
-  }
-)
-
-export const Testimonials = dynamic(
-  () =>
-    import('@/components/sections/testimonials/testimonials').then(
-      (mod) => mod.Testimonials
-    ),
-  {
-    loading: () => <div className="min-h-[300px]" />,
-    ssr: true,
-  }
-)
-
-// Dynamic imports with loading boundaries
-// export const CaseStudies = dynamic(
-//   () =>
-//     import('@/components/sections/case-studies/case-studies').then(
-//       (mod) => mod.CaseStudies
-//     ),
-//   {
-//     loading: () => (
-//       <div className="min-h-[300px] sm:min-h-[350px] lg:min-h-[400px]" />
-//     ),
-//     ssr: true,
-//   }
-// )
 
 export const BlogPosts = dynamic(
   () =>
@@ -110,26 +49,7 @@ export const BlogPosts = dynamic(
   }
 )
 
-export  const FloatVideo = dynamic(
-  () => import('@/components/sections/float-video/float-video').then(mod => mod.FloatVideo),
-  {
-    ssr: true,
-  }
-)
 
-export const BannerModalWrapper = dynamic(
-  () => import('@/components/BannerModalWrapper').then((mod) => mod.BannerModalWrapper),
-  {
-    ssr: false,
-  }
-)
-
-export const TestimonialsSection = dynamic(
-  () => import('@/components/sections/testimonials/testimonials.server'),
-  {
-    ssr: true,
-  }
-)
 
 export const QuoteSection = dynamic(
   () => import('@/components/sections/quote-section/quote-section').then(mod => mod.QuoteSection),
